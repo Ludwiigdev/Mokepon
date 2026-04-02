c8a415a9a90e86fd3dd462a0738ddb1485f8a9d4
Luis Fernandez <luiscode226@gmail.com>
2026-04-01 21:18:41 -0500

    feat: implement Node.js API integration and frontend-backend communication

--- js/mokepon.js
+++ js/mokepon.js
@@ -190,6 +190,20 @@ function iniciarJuego() {
   botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 
   botonReiniciar.addEventListener('click', reiniciarJuego);
+
+  unirseAljuego();
+}
+
+function unirseAljuego() {
+  fetch('http://localhost:8080/unirse')
+  .then(function (res){
+    if(res.ok){
+      res.text()
+      .then(function(respuesta){
+        console.log(respuesta);
+      })
+    }
+  })
 }
 
 function seleccionarMascotaJugador() {
